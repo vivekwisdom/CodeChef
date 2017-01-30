9e807082fdbac2bafc94a09e2f5209c0f702619e
Vivek Wisdom <vivek.wisdom@accenture.com>
2017-01-30 06:03:00 -0500

    Practice Solutions

---
+++ src/vw/codechef/practice/easy/LifeUniverseAndEverything.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package vw.codechef.practice.easy;
+
+/**
+ * @author vivek
+ *
+ */
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * @author vivek.wisdom
+ *
+ */
+public class LifeUniverseAndEverything implements Closeable {
+	private InputReader in = new InputReader(System.in);
+	private PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+	/*
+	 * The main problem solution method.
+	 */
+	public void solve() {
+		int n;
+		while ((n = in.nextInt()) != 42) {
+			out.println(n);
+		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		in.close();
+		out.close();
+	}
+
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
+
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		public void close() throws IOException {
+			reader.close();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		try (LifeUniverseAndEverything instance = new LifeUniverseAndEverything()) {
+			instance.solve();
+		}
+	}
+}
---
+++ src/vw/codechef/practice/easy/UncleJohny.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package vw.codechef.practice.easy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author vivek.wisdom
+ *
+ */
+public class UncleJohny {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int testCases = in.nextInt();
+		for (int i = 0; i < testCases; i++) {
+			int n = in.nextInt();
+			int[] arr = new int[n];
+			for (int j = 0; j < n; j++) {
+				arr[j] = in.nextInt();
+			}
+			int k = in.nextInt();
+			int ni = arr[k - 1];
+			Arrays.sort(arr);
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[j] == ni) {
+					System.out.println(j + 1);
+				}
+			}
+		}
+
+	}
+
+}
